8cd6898c9363a4cd1e9bc98599a34f89ec78b1ff
서찬희 <chanh@seochanhuiui-MacBookPro.local>
2020-09-02 23:57:22 +0900

    200902 2358 css division

--- js/page-transition.js
+++ js/page-transition.js
@@ -3,11 +3,15 @@ const aboutButton = document.querySelector(".menu__about");
 const worksButton = document.querySelector(".menu__works");
 
 aboutButton.addEventListener('click', function() {
-  const about = document.querySelector(".about");
-  about.classList.toggle("active");
+  const aboutPage = document.querySelector(".about");
+  const aboutTitle = document.querySelector(".menu__about-box")
+  aboutPage.classList.toggle("active");
+  aboutTitle.classList.toggle("active");
 });
 
 worksButton.addEventListener('click', function() {
-  const works = document.querySelector(".works");
-  works.classList.toggle("active");
+  const worksPage = document.querySelector(".works");
+  const worksTitle = document.querySelector(".menu__works-box");
+  worksPage.classList.toggle("active");
+  worksTitle.classList.toggle("active");
 });
